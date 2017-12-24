c99d40c8ac6aabbdc167fdf7986fefcf0d6a68c7
Brittany Hartmire <bkhartmire@gmail.com>
2017-12-24 06:44:15 +0000

    automatically backed up by learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -12,7 +12,7 @@ class PostsController < ApplicationController
 	end
 
 	def create
-	  @post = Post.new(params["post"])
+	  @post = Post.new(post_params)
 	  @post.save
 	  redirect_to post_path(@post)
 	end
@@ -28,7 +28,7 @@ class PostsController < ApplicationController
 	end
 
 	private
- 
+
 	def post_params
 	  params.require(:post).permit(:title, :description)
 	end
